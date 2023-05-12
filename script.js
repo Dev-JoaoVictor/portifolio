939ed9b3295bd8babfa00b8e97dd845d9308f745
João <joaovictor.ramos34@gmail.com>
2023-05-12 17:04:10 -0300

    trocando a imagem ao fechar o menu

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const btnMenu = document.querySelector('.btn-menu')
 
+
 function toggleMenu() {
+  const btnOpen = document.querySelector('.btn-open')
+  const btnClose = document.querySelector('.btn-close')
   const nav = document.querySelector('nav')
   nav.classList.toggle('active')
+  btnOpen.classList.toggle('hidden')
+  btnClose.classList.toggle('hidden')
+
 }
 
 
